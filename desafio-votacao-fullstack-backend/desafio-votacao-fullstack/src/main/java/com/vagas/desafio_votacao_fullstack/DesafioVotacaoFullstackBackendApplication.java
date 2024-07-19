6f5459c0184bc87a9611fdacf9c65f280f3fffb3
Gabriel Galindo <gabrielgalindo1m@gmail.com>
2024-07-18 23:45:06 -0300

    feat (unversioned files) : this commit contains unversioned files like .xml, .cmd and others

---
+++ desafio-votacao-fullstack-backend/desafio-votacao-fullstack/src/main/java/com/vagas/desafio_votacao_fullstack/DesafioVotacaoFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vagas.desafio_votacao_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoFullstackBackendApplication.class, args);
+	}
+
+}
---
+++ desafio-votacao-fullstack-backend/desafio-votacao-fullstack/src/test/java/com/vagas/desafio_votacao_fullstack/DesafioVotacaoFullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagas.desafio_votacao_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoFullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
